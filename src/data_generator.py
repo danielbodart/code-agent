4f5ff25f17d440dc6f199b2e44083c2d113a44fc
Daniel Bodart <dan@bodar.com>
2025-03-22 16:21:15 +0000

    Update generate example to using reasoning

--- src/data_generator.py
+++ src/data_generator.py
@@ -1,11 +1,16 @@
 import random
+from src.reasoning_example import ReasoningExample
 
 def generate_addition_example(r = random):
     a = r.randint(0, 10)
     b = r.randint(0, 10)
-    return [f"Question: What is {a} + {b}?", 
-            f"Reasoning: If {a} = {number_as_ones(a)},\nAnd {b} = {number_as_ones(b)}\nThen {a} + {b} = {number_as_ones(a + b)}\nTherefore {a} + {b} = {a + b}", 
-            f"Answer: {a + b}"]
+    return ReasoningExample(f"What is {a} + {b}?", [
+                f"If {a} = {number_as_ones(a)}", 
+                f"And {b} = {number_as_ones(b)}",
+                f"Then {a} + {b} = {number_as_ones(a + b)}", 
+                f"Therefore {a} + {b} = {a + b}"
+            ], 
+            f"{a + b}")
 
 def number_as_ones(n):
     if n == 0:
--- test/test_data_generator.py
+++ test/test_data_generator.py
@@ -5,9 +5,9 @@ from random import Random
 class TestDataGenerator(unittest.TestCase):
     def test_generate_addition_example(self):
         example = generate_addition_example(Random(1))
-        self.assertEqual(example[0], "Question: What is 2 + 9?")
-        self.assertEqual(example[1], "Reasoning: If 2 = 1 + 1,\nAnd 9 = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1\nThen 2 + 9 = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1\nTherefore 2 + 9 = 11")
-        self.assertEqual(example[2], "Answer: 11")
+        self.assertEqual(example.question, "What is 2 + 9?")
+        self.assertEqual(example.reasoning_steps, ["If 2 = 1 + 1", "And 9 = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1", "Then 2 + 9 = 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1","Therefore 2 + 9 = 11"])
+        self.assertEqual(example.answer, "11")
 
 if __name__ == '__main__':
     unittest.main()
